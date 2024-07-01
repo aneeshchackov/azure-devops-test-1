401a8ed78e798cb273a1ec764c24f369af507391
Aneesh Chacko V <aneesh.us@gmail.com>
2024-07-01 18:44:11 +0530

    Added lombok

--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -1,20 +1,14 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Greeting {
 
 	private final long id;
+
 	private final String content;
 
-	public Greeting(long id, String content) {
-		this.id = id;
-		this.content = content;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public String getContent() {
-		return content;
-	}
 }
